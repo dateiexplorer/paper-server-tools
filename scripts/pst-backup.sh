fdd79bf54fdeaae6c5f7a52f17e34530891779ff
Justus Röderer <justus.roederer@outlook.com>
2021-02-14 02:29:52 +0100

    Fixed pst-backup.sh

--- scripts/pst-backup.sh
+++ scripts/pst-backup.sh
@@ -2,6 +2,8 @@
 # Run this script to backup a server.
 # It creates a complete copy of the server instance.
 
+current_pwd="$(pwd)"
+
 server="$1"
 if [ -z "$server" ]; then
     echo "Please enter name of server you want to backup."
@@ -14,7 +16,7 @@ if ! [ -d "$PAPER_HOME/server/$server" ]; then
 fi
 
 # Get current timestamp as foldername
-timestamp="$(date + "%Y%m%d%H%M")"
+timestamp="$(date +"%Y%m%d%H%M")"
 path="$PAPER_BACKUP/$server"
 
 # Make dirs if they're not created yet.
@@ -23,6 +25,8 @@ mkdir -p "$path"
 echo "Create backup from $server with timestamp $timestamp"
 
 # Creates a tar archive and compress it with gzip
-tar -czpf "$path/$timestamp.tar.gz" "$PAPER_HOME/server/$server"
+cd "$PAPER_HOME/server/"
+tar -czvpf "$path/$timestamp.tar.gz" "$server"
 
 echo "Process finished successfully!"
+cd "$current_pwd"

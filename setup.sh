95d7a07764dae97be6dceba61f107f953b0a068c
Justus Röderer <justus.roederer@outlook.com>
2021-05-20 18:44:13 +0200

    Improve setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Run this script to setup your system for the paper-server-tools.
+# It checks all requirements for the toolchain and setup some environment
+# variables in the ~/.profile file.
 
+# Show banner
 echo -e "
 Welcome to the  __                 __
 ______  _______/  |_  ____   ____ |  |   ______
@@ -14,39 +17,66 @@ ______  _______/  |_  ____   ____ |  |   ______
 # Check requirements for execution
 echo "Checking requirements..."
 
+# Add all required packages below
+all_requirements_present=true
 for requirement in tar gzip curl sed jq java screen; do
-    if ! [ -x "$(command -v $requirement)" ]; then
-        echo -e "\
-The following programm is required, but missing:
-    $requirement
-Please install it and restart the setup.
-"
-        exit -1
+    if [ -x "$(command -v $requirement)" ]; then
+        echo "  [*] $requirement"
+    else
+        echo "  [ ] $requirement"
+        unset all_requirements_present
     fi
 done
 
-echo "All requirements are installed. Let's go!"
+# Check if requirement is missing (requirement_is_missing is set)
+if [ -z $all_requirements_present ]; then
+    echo "Some requirements [ ] are missing."
+    echo "Please install them and rerun the setup."
+    exit 0
+else
+    echo "All requirements are installed. Let's go!"
+fi
+
 printf "\n"
 
+# Set environment file
+env_file="$HOME/.profile"
 
-if [ -f "$HOME/.bash_profile" &> /dev/null ] && [ $(cat "$HOME/.bash_profile" \
+# Check if $env_file and environment variables already exists.
+if [ -f "$env_file" &> /dev/null ] && [ $(cat "$env_file" \
         | grep -w "PAPER_HOME" | wc -l) -ne 0 ]; then
     echo "PAPER_HOME is already set:"
-    echo "    $(cat $HOME/.bash_profile | grep -w PAPER_HOME | cut -d = -f2)"
-    echo "You can change the path by edit this variable in the ~/.bash_profile \
-file."
+    echo "  $(cat "$env_file" | grep -w PAPER_HOME | cut -d = -f2)"
+    echo "You can change the path by edit this environment variable in \
+$env_file."
 else
-    echo "Set PAPER_HOME variable in ~/.bash_profile"
-    echo "PAPER_HOME=\"$HOME/Paper/server\"" >> "$HOME/.bash_profile"
+    echo "Set PAPER_HOME variable in $env_file:"
+    echo "  PAPER_HOME=\"$HOME/Paper/server\""
+
+    # Write to $env_file
+    echo "export PAPER_HOME=\"$HOME/Paper/server\"" >> "$env_file"
 fi
 
 printf "\n"
-if [ $(cat "$HOME/.bash_profile" | grep -w "PAPER_BACKUP" | wc -l) -ne 0 ]; then
+
+# Check if PAPER_BACKUP environment variable is set.
+if [ $(cat $env_file | grep -w "PAPER_BACKUP" | wc -l) -ne 0 ]; then
     echo "PAPER_BACKUP is already set:"
-    echo "    $(cat $HOME/.bash_profile | grep -w "PAPER_BACKUP" | cut -d = -f2)"
-    echo "You can change the path by edit this variable in the ~/.bash_profile \
-file."
+    echo "  $(cat "$env_file" | grep -w "PAPER_BACKUP" | cut -d = -f2)"
+    echo "You can change the path by edit this environment variable in \
+$env_file."
 else
-    echo "Set PAPER_BACKUP variable in ~/.bash_profile"
-    echo "PAPER_BACKUP=\"$HOME/Paper/backups\"" >> "$HOME/.bash_profile"
+    echo "Set PAPER_BACKUP variable in $env_file:"
+    echo "  PAPER_BACKUP=\"$HOME/Paper/backups\""
+
+    # Write to $env_file
+    echo "export PAPER_BACKUP=\"$HOME/Paper/backups\"" >> "$env_file"
 fi
+
+# Source the $env_file to apply changes and load environment variables in
+# space.
+printf "\n"
+echo "Source $env_file to load environment variables..."
+source "$env_file"
+
+echo "Finished successfully!"

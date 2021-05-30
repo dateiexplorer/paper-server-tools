74e455aa0160bfe5b32acfe76aeaefc314be60ad
Justus Röderer <justus.roederer@outlook.com>
2021-05-30 20:48:53 +0200

    Refactoring run.sh script for Mincrafter bot

--- minecrafter_bot/run.sh
+++ minecrafter_bot/run.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
+# Print banner
+echo -e "
+                __                 __
+______  _______/  |_  ____   ____ |  |   ______
+\____ \/  ___/\   __\/  _ \ /  _ \|  |  /  ___/
+|  |_> >___ \  |  | (  <_> |  <_> )  |__\___ \\
+|   __/____  > |__|  \____/ \____/|____/____  >
+|__|       \/                               \/
+
+Setting up the newest version of the Minecrafter bot
+"
+
+# Get newest version of the repository
+echo "Get newest version..."
+git pull
+
+# Build bot
+echo "Build bot..."
+cargo build --release
+
 # Stop all other running instances
+echo "Kill running instances of the bot..."
 for process in $(ps -ef | grep "minecrafter_bot" | \
     grep -v "grep" | awk '{ print $2 }'); do
     
     kill "$process"
 done
 
-# Get newest version of the repository
-git pull
-
-# Build bot
-cargo build --release
-
 # Run bot in background
+echo "Running bot..."
 exec ./target/release/minecrafter_bot &> bot.log &
